6f205be05e8e69f413877fdd12c2aae8d9b4fd67
Anderson Pinto <74440689+andersonstpinto@users.noreply.github.com>
2022-01-11 19:44:17 -0300

    Name fix

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ class Card {
   imageUrl() {
     fetch(`https://api.the${this._animal}api.com/v1/images/search`)
       .then(response => response.json())
-      .then(object => (this._image.src = object[0].url))
+      .then(objArray => (this._image.src = objArray[0].url))
   }
 }
 
@@ -19,28 +19,3 @@ const Dog = new Card('dog')
 
 Cat.imageUrl()
 Dog.imageUrl()
-
-/*
-;[
-  {
-    breeds: [
-      {
-        weight: { imperial: '65 - 100', metric: '29 - 45' },
-        height: { imperial: '23 - 25', metric: '58 - 64' },
-        id: 9,
-        name: 'Alaskan Malamute',
-        bred_for: 'Hauling heavy freight, Sled pulling',
-        breed_group: 'Working',
-        life_span: '12 - 15 years',
-        temperament:
-          'Friendly, Affectionate, Devoted, Loyal, Dignified, Playful',
-        reference_image_id: 'dW5UucTIW',
-      },
-    ],
-    id: 'BbMFS3bU-',
-    url: 'https://cdn2.thedogapi.com/images/BbMFS3bU-.jpg',
-    width: 1280,
-    height: 853,
-  }
-]
-*/
